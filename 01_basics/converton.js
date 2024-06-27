26c159640788e5ad0c0ec51b18661f336c52aeda
Istkharahamad <123648084+Istkharahamad@users.noreply.github.com>
2024-06-27 18:00:49 +0000

    conversion of datatypes

---
+++ 01_basics/converton.js
@@ -0,0 +1,12 @@
+//let age=true
+
+//console.log(typeof(age))
+
+//let valueInNumber=Number(age)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+
+let islogged="istnmbkjxchlik"
+let boolesnLoggedIn=Boolean(islogged)
+console.log(boolesnLoggedIn);
