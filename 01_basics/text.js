f212a8ca4bdb1b0f2fd6ca8763305219248086d2
Istkharahamad <123648084+Istkharahamad@users.noreply.github.com>
2024-06-27 18:48:27 +0000

    changed data type

---
+++ 01_basics/text.js
@@ -0,0 +1,11 @@
+console.log("istkhar");
+const accountId= 144553
+let accountEmail="istkhar@gmail.com"
+var accountpass="12345"
+accountcity="jaipur"
+let accounttate;
+
+//accountId=2
+accountcity="Benguroro"
+
+console.table([accountId,accountEmail, accountpass,accountcity,accounttate]);
---
+++ 02_datatype.js
@@ -0,0 +1,13 @@
+'use strict';
+//alert("3+3") we are using node.ja not browser
+
+console.log(4+5);
+let name= "istkhar"
+let age=21
+let isLoggedIn= false
+//Number
+//BigInt
+//Boolean 
+//null
+//undefined
+console.log(typeof 'mukesh')
